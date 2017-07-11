5f4dacb802a061f305311ed7ad4905d46f6afe9c
William Rodrigues de Sousa <rm78369@fiap.com.br>
2017-07-10 23:01:43 -0300

    Update site.js

--- js/site.js
+++ js/site.js
@@ -4,11 +4,11 @@ var tela = document.querySelector('canvas');
 var pincel = tela.getContext('2d');
 
 //Função que recebe a cor da tinta para pintar o fundo do Canvas
- function recebeTinta(corfundo) {
-     var tinta = document.querySelector('input[id="colorPickerBackground"]');
-     var cor = tinta.value;
-      pincel.fillStyle = cor;
-      pincel.fillRect(0, 0, 1045,450);
+function recebeTinta(corfundo) {
+    var tinta = document.querySelector('input[id="colorPickerBackground"]');
+    var cor = tinta.value;
+    pincel.fillStyle = cor;
+    pincel.fillRect(0, 0, 1045,450);
 }
 
 //Função que recebe a cor da tinta para pintar o pincel e pintar o círculo no Canvas
@@ -19,19 +19,29 @@ function corPincel(cor) {
     pincel.fillStyle = cor;
 }
 
+var desenha = false;
+
 // Função que permite pintar o Círculo no Canvas
-function desenhaCirculo(evento,tamanho) {
-    
-    var x = evento.pageX - tela.offsetLeft;
-    var y = evento.pageY - tela.offsetTop;
-    var tamanho = document.querySelector('input[id="tamFonte"]');
-    var tam = tamanho.value;
 
-    corPincel();
-    pincel.beginPath();
-    pincel.arc(x, y, tam, 0, 2*3.14);
-    pincel.fill();
-}
+tela.onmousemove = function(evento) {
+    if(desenha) {
+        var x = evento.pageX - tela.offsetLeft;
+        var y = evento.pageY - tela.offsetTop;
+        var tamanho = document.querySelector('input[id="tamTraco"]');
+        var tam = tamanho.value;
 
-tela.onclick = desenhaCirculo; //Funcionalidade que permite desenhar o círculo ao clicar no canvas.
+        corPincel();
+        pincel.beginPath();
+        pincel.arc(x, y, tam, 0, 2 * 3.14);
+        pincel.fill();
+    }
+} //Funcionalidade que permite desenhar o círculo ao clicar no canvas.
 pintarFundo.onclick = recebeTinta; //Funcionalidade que permite alterar a cor do fundo do canvas, ao clicar no botão de Pintar Fundo
+
+
+tela.onmousedown = function() {
+    desenha = true;
+};
+tela.onmouseup = function(){
+    desenha = false;
+};
